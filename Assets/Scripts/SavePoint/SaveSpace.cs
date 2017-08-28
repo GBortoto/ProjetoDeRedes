e189b8a6bb67613e1be52ca0527cd778fe84e785
GBortoto <guilherme.bortoto.moraes@usp.br>
2017-08-27 21:08:41 -0300

    SavePoint

---
+++ Assets/Scripts/SavePoint/SaveSpace.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveSpace : MonoBehaviour {
+
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		player = other.gameObject;
+		Debug.Log (player.name + " Entrou");
+	}
+
+	void OnTriggerExit(Collider other){
+		player = other.gameObject;
+		Debug.Log (player.name + " Saiu");
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
